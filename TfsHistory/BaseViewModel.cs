94abd8cb57e4d6c4d78e78cefeb223706c77f961
titte <titte@telia.com>
2015-12-07 13:31:51 +0100

    Added initial version

---
+++ TfsHistory/BaseViewModel.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TfsHistory
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public static string Q3Path = "";
+        /// <summary>
+        /// Raised when a property on this object has a new value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        ///// <summary>
+        /////     Raise a property change and infer the frame from the stack
+        ///// </summary>
+        ///// <remarks>
+        /////     May not work on some systems (64-bit, for example, not yet supported by Silverlight). 
+        ///// </remarks>
+        //[MethodImpl(MethodImplOptions.NoInlining)]
+        //public virtual void RaisePropertyChanged()
+        //{
+        //    var frames = new System.Diagnostics.StackTrace();
+        //    for (var i = 0; i < frames.FrameCount; i++)
+        //    {
+        //        var frame = frames.GetFrame(i).GetMethod() as MethodInfo;
+        //        if (frame != null)
+        //            if (frame.IsSpecialName && frame.Name.StartsWith("set_"))
+        //            {
+        //                RaisePropertyChanged(frame.Name.Substring(4));
+        //                return;
+        //            }
+        //    }
+        //    throw new InvalidOperationException("NotifyPropertyChanged() can only by invoked within a property setter.");
+        //}
+
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event for each of the properties.
+        /// </summary>
+        /// <param name="propertyNames">The properties that have a new value.</param>
+        protected void RaisePropertyChanged(params string[] propertyNames)
+        {
+            foreach (var name in propertyNames)
+            {
+                RaisePropertyChanged(name);
+            }
+        }
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <typeparam name="T">The type of the property that has a new value</typeparam>
+        /// <param name="propertyExpression">A Lambda expression representing the property that has a new value.</param>
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = ExtractPropertyName(propertyExpression);
+            RaisePropertyChanged(propertyName);
+        }
+
+        protected string ExtractPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("Property not a member", "propertyExpression");
+            }
+
+            var property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new ArgumentException("Not a propery", "propertyExpression");
+            }
+
+            var getMethod = property.GetGetMethod(true);
+
+            if (getMethod == null)
+            {
+                // this shouldn't happen - the expression would reject the property before reaching this far
+                throw new ArgumentException("No getter", "propertyExpression");
+            }
+
+            if (getMethod.IsStatic)
+            {
+                throw new ArgumentException("Static property", "propertyExpression");
+            }
+
+            return memberExpression.Member.Name;
+        }
+
+
+        /// <summary>
+        ///     Internal list of errors
+        /// </summary>
+        private readonly Dictionary<string, IEnumerable<string>> _errors = new Dictionary<string, IEnumerable<string>>();
+
+        /// <summary>
+        ///     Collction of errors changed
+        /// </summary>
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        ///// <summary>
+        ///// Occurs when the validation errors have changed for a property or for the entire object.
+        ///// </summary>
+        //event EventHandler<DataErrorsChangedEventArgs> INotifyDataErrorInfo.ErrorsChanged
+        //{
+        //    [method:
+        //        SuppressMessage("Microsoft.Design", "CA1033", Justification = "Doesn't need to access event methods.")]
+        //    add { ErrorsChanged += value; }
+        //    [method:
+        //        SuppressMessage("Microsoft.Design", "CA1033", Justification = "Doesn't need to access event methods.")]
+        //    remove { ErrorsChanged -= value; }
+        //}
+
+        ///// <summary>
+        /////     True if errors exist
+        ///// </summary>
+        //bool INotifyDataErrorInfo.HasErrors
+        //{
+        //    get { return HasErrors; }
+        //}
+
+        /// <summary>
+        ///     True if errors exist
+        /// </summary>
+        public bool HasErrors
+        {
+            get { return _errors.Any(); }
+        }
+
+        ///// <summary>
+        /////     Get errors for a property
+        ///// </summary>
+        ///// <param name="propertyName">The property</param>
+        ///// <returns>The list of errors</returns>
+        //IEnumerable INotifyDataErrorInfo.GetErrors(string propertyName)
+        //{
+        //    return GetErrors(propertyName);
+        //}
+
+        /// <summary>
+        ///     Get errors for a property
+        /// </summary>
+        /// <param name="propertyName">The name of the property</param>
+        /// <returns></returns>
+        protected virtual IEnumerable GetErrors(string propertyName)
+        {
+            IEnumerable<string> error;
+            return _errors.TryGetValue(propertyName ?? string.Empty, out error) ? error : null;
+        }
+
+        /// <summary>
+        ///     Set an error for a property
+        /// </summary>
+        /// <param name="propertyName">The name of the property</param>
+        /// <param name="error">The error</param>
+        public virtual void SetError(string propertyName, string error)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                return;
+            SetErrors(propertyName, new List<string> { error });
+        }
+
+        /// <summary>
+        ///     Overload for expression
+        /// </summary>
+        /// <typeparam name="T">The type of the property</typeparam>
+        /// <param name="propertyExpresssion">An expression that points to the property</param>
+        /// <param name="error">The error</param>
+        protected virtual void SetError<T>(Expression<Func<T>> propertyExpresssion, string error)
+        {
+            var propertyName = ExtractPropertyName(propertyExpresssion);
+            SetError(propertyName, error);
+        }
+
+        /// <summary>
+        ///     Clears the errors
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public virtual void ClearErrors(string propertyName)
+        {
+            SetErrors(propertyName, new List<string>());
+        }
+
+        /// <summary>
+        ///     Clear all errors for a property
+        /// </summary>
+        /// <typeparam name="T">The type of the property</typeparam>
+        /// <param name="propertyExpresssion">The expression that points to the property</param>
+        protected virtual void ClearErrors<T>(Expression<Func<T>> propertyExpresssion)
+        {
+            var propertyName = ExtractPropertyName(propertyExpresssion);
+            ClearErrors(propertyName);
+        }
+
+        /// <summary>
+        ///     Set errors for a property
+        /// </summary>
+        /// <typeparam name="T">The type of the property</typeparam>
+        /// <param name="propertyExpresssion">The expression for the property</param>
+        /// <param name="propertyErrors">The collection of errors</param>
+        protected virtual void SetErrors<T>(Expression<Func<T>> propertyExpresssion, IEnumerable<string> propertyErrors)
+        {
+            var propertyName = ExtractPropertyName(propertyExpresssion);
+            SetErrors(propertyName, propertyErrors);
+        }
+
+        /// <summary>
+        ///     Set errors for a property
+        /// </summary>
+        /// <param name="propertyName">The name of the property</param>
+        /// <param name="propertyErrors">The collection of errors</param>
+        protected virtual void SetErrors(string propertyName, IEnumerable<string> propertyErrors)
+        {
+            if (propertyErrors.Any(error => error == null))
+            {
+                throw new ArgumentException("No null errors", "propertyErrors");
+            }
+
+            var propertyNameKey = propertyName ?? string.Empty;
+
+            IEnumerable<string> currentPropertyErrors;
+            if (_errors.TryGetValue(propertyNameKey, out currentPropertyErrors))
+            {
+                if (!_AreErrorCollectionsEqual(currentPropertyErrors, propertyErrors))
+                {
+                    if (propertyErrors.Any())
+                    {
+                        _errors[propertyNameKey] = propertyErrors;
+                    }
+                    else
+                    {
+                        _errors.Remove(propertyNameKey);
+                    }
+
+                    RaiseErrorsChanged(propertyNameKey);
+                }
+            }
+            else
+            {
+                if (propertyErrors.Any())
+                {
+                    _errors[propertyNameKey] = propertyErrors;
+                    RaiseErrorsChanged(propertyNameKey);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Raises this object's ErrorsChangedChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has new errors.</param>
+        protected virtual void RaiseErrorsChanged(string propertyName)
+        {
+            var handler = ErrorsChanged;
+            if (handler != null)
+            {
+                handler(this, new DataErrorsChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        ///     Raises this object's ErrorsChangedChanged event.
+        /// </summary>
+        /// <typeparam name="T">The type of the property that has errors</typeparam>
+        /// <param name="propertyExpresssion">A Lambda expression representing the property that has new errors.</param>
+        protected virtual void RaiseErrorsChanged<T>(Expression<Func<T>> propertyExpresssion)
+        {
+            var propertyName = ExtractPropertyName(propertyExpresssion);
+            RaiseErrorsChanged(propertyName);
+        }
+
+        /// <summary>
+        ///     Compares error collections
+        /// </summary>
+        /// <param name="propertyErrors">The property errors</param>
+        /// <param name="currentPropertyErrors">The current</param>
+        /// <returns>True if there are/aren't equal</returns>
+        private static bool _AreErrorCollectionsEqual(IEnumerable<string> propertyErrors,
+                                                      IEnumerable<string> currentPropertyErrors)
+        {
+            var equals = currentPropertyErrors.Zip(propertyErrors, (current, newError) => current == newError);
+            return propertyErrors.Count() == currentPropertyErrors.Count() && equals.All(b => b);
+        }
+    }
+
+    public class Info
+    {
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public SolidColorBrush Color { get; set; }
+    }
+}
---
+++ TfsHistory/MainWindow.xaml.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TfsHistory
+{
+
+    // Input params: repo, branch, startdate
+    // Start with filename, changeset, date
+
+    // Print filename, backlog item, date of change
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel  _vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new ViewModel();
+            DataContext = _vm;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            TfsChangedFiles();
+        }
+
+        private void TfsChangedTasks()
+        {
+            TfsHelper TFS = new TfsHelper(StatusText);
+            var startDate = DateTime.Parse(DateText.Text);
+            //List<ChangeSetInfo> info = TFSChangelogGenerator.GetChangeinfo(teamProjectCollection, versionControlServer, buildServer, "$/Securitas GSP/Releases/Release1.28", "Ref", null, "T");
+            var changes = TFS.GetChangedFiles(startDate, @"C:\Source Control\TFS\Securitas GSP\Main", "1",
+                "http://tfs.stratiteq.se/tfs/Stratiteq",
+                "true", "false");
+            _vm.ChangedFiles = new ObservableCollection<TfsHistoryItem>(changes);
+            StatusText.Text = "Done!";
+        }
+
+        private void TfsChangedFiles()
+        {
+            StatusText.Text = "Contacting TFS";            
+            // Arguments 
+            // args[0]</code> // local repository path
+            // args[1]</code> // Change sheet #(you may go with change sheet number between 1-24000+ of a date) 
+            // args[2]</code> // your remote TFS collections URL
+            // args[3]</code> // true or false - get list of concatenated "File Name+Date+change Type"
+            // args[4]</code> // true or false - absolute path of the file? true, else only file name
+
+            TfsHelper TFS = new TfsHelper(StatusText);
+            var startDate = DateTime.Parse(DateText.Text);
+            //List<ChangeSetInfo> info = TFSChangelogGenerator.GetChangeinfo(teamProjectCollection, versionControlServer, buildServer, "$/Securitas GSP/Releases/Release1.28", "Ref", null, "T");
+            var changes = TFS.GetChangedFiles(startDate, @"C:\Source Control\TFS\Securitas GSP\Main", "1",
+                "http://tfs.stratiteq.se/tfs/Stratiteq",
+                "true", "false");                       
+            _vm.ChangedFiles = new ObservableCollection<TfsHistoryItem>(changes);
+            StatusText.Text = "Done!";
+        }
+
+        private void Export_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fileName = @"C:\Temp\TFS\TfsHistory.txt";
+            StatusText.Text = "Starting export";            
+            using (StreamWriter file = new StreamWriter(fileName, false))
+            {
+                file.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6}", "CheckInTime", "Filename", "ChangesetId", "Task", "WorkItem", "WorkItemType", "WorkItemTitle"));
+                foreach (var changedFile in _vm.ChangedFiles)
+                {
+                    file.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6}", changedFile.CheckInTime.ToShortDateString(), changedFile.FileName, changedFile.ChangeSetId, changedFile.GetTaskText, changedFile.GetWorkItemInfo, changedFile.WorkItemType, changedFile.WorkItemTitle) );
+                }                
+            }
+            StatusText.Text = string.Format("Export written to file: {0}", fileName);
+        }
+
+        private void SearchNoFiles_OnClick(object sender, RoutedEventArgs e)
+        {
+            TfsChangedTasks();
+        }
+    }
+
+    public class TfsHistoryItem
+    {
+        public string FileName { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public string CheckedInBy { get; set; }
+        public int ChangeSetId { get; set; }
+        public IEnumerable<AssociatedWorkItemInfo> WorkItems { get; set; }
+
+        public WorkItem PrimaryWorkItem { get; set; }
+
+        public string GetTaskText
+        {
+            get
+            {
+                var associatedWorkItemInfo = WorkItems.FirstOrDefault(x => x.WorkItemType == "Task");
+                if (associatedWorkItemInfo != null)
+                    return associatedWorkItemInfo.Title;
+                else
+                {
+                    return "";
+                }                 
+            }
+        }
+
+        public string GetWorkItemInfo
+        {
+            get { return PrimaryWorkItem?.Id.ToString() ?? "No associated workitems"; }
+            //get { return WorkItems != null ? String.Join(":", WorkItems.Select(x => x.Id).FirstOrDefault()) : "No associated workitems"; }
+        }
+
+        public string WorkItemType
+        {
+            get { return PrimaryWorkItem?.Type.Name ?? ""; }
+            //get { return WorkItems != null ? WorkItems.Select(x => x.WorkItemType).FirstOrDefault() : ""; }
+        }
+
+        public string WorkItemTitle
+        {
+            get { return PrimaryWorkItem?.Title ?? ""; }
+            //get { return WorkItems != null ? WorkItems.Select(x => x.Title).FirstOrDefault() : ""; }
+        }
+    }
+
+    public class ViewModel : BaseViewModel
+    {
+        private ObservableCollection<TfsHistoryItem> _changedFiles;
+
+        public ObservableCollection<TfsHistoryItem> ChangedFiles
+        {
+            get { return _changedFiles; }
+            set { _changedFiles = value; RaisePropertyChanged(() => ChangedFiles); }
+        }
+    }
+
+    public class TfsHelper
+    {
+        private TextBox _status;
+
+        public TfsHelper(TextBox status)
+        {
+            _status = status;
+        }
+
+        private void Log(string message)
+        {
+            _status.Text = message;
+        }
+
+        public IList<TfsHistoryItem> GetChangedTasks(DateTime startDate, params string[] args)
+        {
+            List<TfsHistoryItem> changedFiles = new List<TfsHistoryItem>();
+            string localPath = args[0];
+            string versionFromString = args[1];
+            TfsTeamProjectCollection tfs = null;
+            if (args.Length > 2)
+            {
+                tfs = new TfsTeamProjectCollection(new Uri(args[2]));
+            }
+            else return null;
+            VersionControlServer vcs = tfs.GetService<VersionControlServer>();
+            String[] requestedWorkItemTypes = {"Product Backlog Item", "Bug"};
+            Log("Login to TFS succesful");
+            try
+            {
+                var workItemStore = tfs.GetService<Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore>();
+                //workItemStore.Query()
+
+
+                var changeSetItems = vcs.QueryHistory(localPath,
+                                        VersionSpec.ParseSingleSpec(
+                                                          versionFromString,
+                                                          null),
+                                        0, RecursionType.Full, null,
+                                        VersionSpec.ParseSingleSpec(
+                                                          versionFromString,
+                                                          null),
+                                                          null, Int32.MaxValue, true, false);
+                foreach (Changeset item in changeSetItems)
+                {
+                    if (item.Changes == null || !item.Changes.Any())
+                    {
+                        WorkItem workItem = null;
+                        var primaryWorkItem = item.WorkItems?.FirstOrDefault(t => t.Type.Name != "Task");
+                        if (primaryWorkItem != null)
+                        {
+                            workItem = primaryWorkItem;
+                        }
+                        if (item.WorkItems != null)
+                        {
+                            var task = item.WorkItems?.FirstOrDefault(t => t.Type.Name == "Task");
+                            if (task != null)
+                            {
+                                workItem = GetTaskWorkItemParents(workItemStore, requestedWorkItemTypes, task).FirstOrDefault();
+                            }
+                        }
+
+                        changedFiles.Add(new TfsHistoryItem() { FileName = "", CheckInTime = item.CreationDate, ChangeSetId = item.ChangesetId, WorkItems = item.AssociatedWorkItems?.ToList(), PrimaryWorkItem = workItem });
+                    }                    
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return changedFiles;
+        }
+    
+
+    public IList<TfsHistoryItem> GetChangedFiles(DateTime startDate, params string[] args)
+        {            
+            List<TfsHistoryItem> changedFiles = new List<TfsHistoryItem>();
+            string localPath = args[0];
+            string versionFromString = args[1];
+            TfsTeamProjectCollection tfs = null;
+            if (args.Length > 2)
+            {
+                tfs = new TfsTeamProjectCollection(new Uri(args[2]));
+            }
+            else return null;
+            VersionControlServer vcs = tfs.GetService<VersionControlServer>();
+            String[] requestedWorkItemTypes = { "Product Backlog Item", "Bug" };
+            Log("Login to TFS succesful");
+            try
+            {
+                var workItemStore = tfs.GetService<Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore>();
+                var changeSetItems = vcs.QueryHistory(localPath,
+                                                      VersionSpec.ParseSingleSpec(
+                                                                        versionFromString,
+                                                                        null),
+                                                      0, RecursionType.Full, null,
+                                                      VersionSpec.ParseSingleSpec(
+                                                                        versionFromString,
+                                                                        null),
+                                                                        null, Int32.MaxValue, true, false);
+                Log("Query completed, starting to enumerate!");
+                int itemCount = 0;
+                foreach (Changeset item in changeSetItems)
+                {
+                    DateTime checkInDate = item.CreationDate;
+                    if (checkInDate < startDate) continue;                    
+
+                    string user = item.Committer;
+                    foreach (Change changedItem in item.Changes)
+                    {
+                        string filename = changedItem.Item.ServerItem.Substring
+                        (changedItem.Item.ServerItem.LastIndexOf('/') + 1);
+                        // Your choice of filters. In this case I was not interested in the below files.
+                        if (!filename.EndsWith(".dll")
+                            && !filename.EndsWith(".pdb")
+                            && !filename.EndsWith(".csproj")
+                             && !filename.EndsWith(".pubxml")
+                              && !filename.EndsWith(".sln")
+                               && !filename.EndsWith(".config")
+                               && !filename.EndsWith(".log")
+                               && !filename.EndsWith(".resx")
+                            && filename.IndexOf(".") > -1
+                            && changedItem.ChangeType.Equals(ChangeType.Edit))
+                        {
+                             
+                            WorkItem workItem = null;                            
+                            var primaryWorkItem = item.WorkItems?.FirstOrDefault(t => t.Type.Name != "Task");
+                            if (primaryWorkItem != null)
+                            {
+                                workItem = primaryWorkItem;
+                            }
+                            if (item.WorkItems != null)
+                            {
+                                var task = item.WorkItems?.FirstOrDefault(t => t.Type.Name == "Task");
+                                if (task != null)
+                                {
+                                    workItem = GetTaskWorkItemParents(workItemStore, requestedWorkItemTypes, task).FirstOrDefault();
+                                }
+                            }
+                            
+                            changedFiles.Add(new TfsHistoryItem() {FileName = filename, CheckInTime = changedItem.Item.CheckinDate, ChangeSetId = changedItem.Item.ChangesetId, WorkItems = item.AssociatedWorkItems?.ToList(), PrimaryWorkItem = workItem});
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            if (changedFiles != null && changedFiles.Count > 0)
+                Console.WriteLine
+                ("-----------------------------------------\nTotal File count: " +
+                changedFiles.Count);
+            Console.WriteLine
+            ("-----------------------------------------\nPress any key to close");
+            //Console.ReadKey();
+            return changedFiles;
+        }
+
+
+        private static List<WorkItem> GetTaskWorkItemParents(Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore workItemStore, String[] requestedWorkItemTypes, Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItem workItem)
+        {
+            List<WorkItem> retval = new List<WorkItem>();
+
+            // Find related Bug or Product Backlog Item
+            foreach (var link in workItem.Links)
+            {
+                Microsoft.TeamFoundation.WorkItemTracking.Client.RelatedLink relLink = link as Microsoft.TeamFoundation.WorkItemTracking.Client.RelatedLink;
+                if (relLink != null && relLink.LinkTypeEnd != null && relLink.LinkTypeEnd.Name == "Parent")
+                {
+                    Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItem wi = workItemStore.GetWorkItem(relLink.RelatedWorkItemId);
+                    if (wi != null && requestedWorkItemTypes.Contains(wi.Type.Name))
+                    {
+                        retval.Add(wi);
+                    }
+                }
+            }
+
+            return retval;
+        }
+
+    }
+}
